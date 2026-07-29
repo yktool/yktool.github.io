bc8fc1668f0105185085687a1ac8769d342f7911
yktools <taruroute1103@gmail.com>
2026-07-29 21:50:48 +0900

    Update SkillRoll.mjs

--- assets/js/SkillRoll.mjs
+++ assets/js/SkillRoll.mjs
@@ -3,9 +3,14 @@ export function SkillRoll(map,path,SkillLocks){
       result = [];
   if(SkillLocks){
     if(Object.prototype.toString.call(SkillLocks) !== "[object Object]") throw new Error("引数SkillLocksにはObjectを入れて欲しい。");
-    for (const pos of SkillLocks){
+    for (const pos in SkillLocks){
       if(!target[pos].includes(SkillLocks[pos])) throw new Error("ロックしているスキルが存在しません。");
     };
+    target = target.map(function(candidate){
+      return candidate.filter(function(one){
+        return !(Object.values(SkillLocks).includes(one));
+      })
+    });
     target.forEach(function(candidate,index){
       if(SkillLocks[index]) {
         result.push(SkillLocks[index]);
@@ -14,14 +19,16 @@ export function SkillRoll(map,path,SkillLocks){
       var newSkill;
       do{
         newSkill = candidate[Math.floor(Math.random() * candidate.length)];
-      } while (!result.includes(newSkill));
+      } while (result.includes(newSkill));
+      result.push(newSkill);
     });
   } else {
     target.forEach(function(candidate){
       var newSkill;
       do{
         newSkill = candidate[Math.floor(Math.random() * candidate.length)];
-      } while (!result.includes(newSkill));
+      } while (result.includes(newSkill));
+      result.push(newSkill);
     });
   };
   return result;
